883d02c003561fb6f91a983ce39647a68925e729
Prietse <jhonpritse@gmail.com>
2021-05-24 04:02:13 +0800

    Main Menu UI and place holder levels

---
+++ Assets/Scripts/GameScripts/ButtonsData_Object.cs
@@ -0,0 +1,31 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// [CreateAssetMenu (fileName = "ButtonsData" , menuName = "Scriptable Objects/ButtonsData" ) ]
+//
+// public class ButtonsData_Object : ScriptableObject
+// {
+//     public GameObject mainMenuPanel;
+//     public GameObject startButton;
+//     
+//     
+//     [Space]
+//     public GameObject lourButton;
+//     
+//     
+//     [Space]
+//     public GameObject settingsButton;
+//     public GameObject settingsPanel;
+//     
+//     
+//     
+//     public GameObject exitButton;
+//     public GameObject exitConfirmPanel;
+//     public GameObject yesExitButton;
+//     public GameObject noExitButton;
+//     
+//
+//
+//
+// }
